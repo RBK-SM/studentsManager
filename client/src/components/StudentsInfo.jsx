cd53c245d499f9e2044471fb31a93a0f75527f60
wassim aziza <wassimaziza56@gmail.com>
2023-07-10 12:11:51 +0200

    updated

--- client/src/components/StudentsInfo.jsx
+++ client/src/components/StudentsInfo.jsx
@@ -3,27 +3,15 @@ import axios from 'axios';
 
 function StudentsInfo(props) {
   const [update, setUpdate] = useState(false)
-  const [name, setName] = useState(props.data1.name)
-  const [gender, setGender] = useState(props.data1.gender)
-  const [phone, setPhone] = useState(props.data1.phone)
-  const [email, setEmail] = useState(props.data1.email)
-  const [attendance, setAttendance] = useState(props.data1.attendance)
-  const [image, setImage] = useState(props.data1.image)
-  const [teachersId, setTeachersId] = useState(props.data1.teachers_idteacher)
+  const [name,setName]=useState('')
+  const [gender,setGender]=useState('')
+  const [email,setEmail]=useState('')
+  const [phone,setPhone]=useState('')
+  const [attendance,setAttendance]=useState('')
+  const [image,setImage]=useState('')
 
-  const handleUpdate = () => {
-    const updated = {
-      id: props.data1.id,
-      name:props.data1.name,
-      gender:props.data1.gender,
-      phone:props.data1.phone,
-      email:props.data1.email,
-      attendance:props.data1.attendance,
-      image:props.data1.image,
-      teachers_idteacher: teachersId
-    };
-
-    axios.put(`http://localhost:3000/api/SM/students/${props.data1.id}`, updated)
+  const handleUpdate = (idstudents,teachers_idteacher,name,gender,phone,email,attendance,image) => {
+    axios.put(`http://localhost:3000/api/SM/students/${idstudents}/${teachers_idteacher}`,{name,gender,phone,email,attendance,image})
       .then((res) => {
         console.log(res)
         setUpdate(false)
@@ -34,7 +22,7 @@ function StudentsInfo(props) {
   }
 
   const handleDelete = () => {
-    axios.delete(`http://localhost:3000/api/SM/students/${props.data1.id}`)
+    axios.delete(`http://localhost:3000/api/SM/students/${props.data1.idstudents}`)
       .then((res) => {
         console.log(res)
       })
@@ -52,14 +40,13 @@ function StudentsInfo(props) {
       <center>
         {isEditing ? (
           <div>
-            <input type="text" value={data1.name} onChange={(e)=>setName(e.target.value)} />
-            <input type="text" value={data1.gender} onChange={(e)=>setGender(e.target.value)} />
-            <input type="text" value={data1.phone} onChange={(e)=>setPhone(e.target.value)} />
-            <input type="text" value={data1.email} onChange={(e)=>setEmail(e.target.value)} />
-            <input type="text" value={data1.attendance} onChange={(e)=>setAttendance(e.target.value)} />
-            <input type="text" value={data1.image} onChange={(e)=>setImage(e.target.value)} />
-            <input type="text" value={data1.teachersId} onChange={(e)=>setTeachersId(e.target.value)} />
-            <button onClick={handleUpdate}>Update</button>
+            <input type="text" value={name} onChange={(e)=>setInput(e.target.value)} />
+            <input type="text" value={gender} onChange={(e)=>setGender(e.target.value)} />
+            <input type="text" value={email} onChange={(e)=>setPhone(e.target.value)} />
+            <input type="text" value={phone} onChange={(e)=>setEmail(e.target.value)} />
+            <input type="text" value={attendance} onChange={(e)=>setAttendance(e.target.value)} />
+            <input type="text" value={image} onChange={(e)=>setImage(e.target.value)} />
+            <button onClick={handleUpdate(props.data1.idstudents,props.data1.teachers_idteacher,name,gender,phone,email,attendance,image)} type="submit">Update</button>
           </div>
         ) : (
           <div>
@@ -68,7 +55,7 @@ function StudentsInfo(props) {
             <p>Email: <br />{props.data1.email}</p>
             <p>Attendance: {props.data1.attendance}</p>
             <div>
-              <button className="btn2" onClick={handleUp}>Update info</button>
+              <button className="btn2" onClick={handleUp()}>Update info</button>
               <button className="btn2" onClick={handleDelete}>Delete Info</button>
             </div>
           </div>
